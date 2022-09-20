3236d1e782794c8b9a7761d3b0b0de7fe6f8f8ef
ElasticBottle <winstonyeo99@yahoo.com>
2022-09-20 10:31:28 -0700

    Allow payment for iframe

--- src/lib/CreatePaymentElement.ts
+++ src/lib/CreatePaymentElement.ts
@@ -26,6 +26,7 @@ export class PaperPaymentElement {
     window.addEventListener("message", handler(iframe));
     iframe.src = link.href;
     iframe.id = iframeId;
+    iframe.allow = "payment";
     iframe.setAttribute(
       "style",
       "margin-left:auto; margin-right:auto; height:350px; width:100%; transition-property:all; transition-timing-function:cubic-bezier(0.4, 0, 0.2, 1); transition-duration:150ms;"
