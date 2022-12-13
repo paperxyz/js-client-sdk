597535239ec0d772f027f3fb7b105273c1c586d2
Phillip Ho <arcoraven@gmail.com>
2022-12-12 19:49:24 -0800

    Set PAPER_APP_URL to withpaper.com

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,17 +1,8 @@
 export const PAPER_APP_URL =
-	process.env.NEXT_PUBLIC_NODE_ENV === "development" ||
-	process.env.NODE_ENV === "development"
+	process?.env?.NEXT_PUBLIC_NODE_ENV === "development" ||
+	process?.env?.NODE_ENV === "development"
 		? "http://localhost:3000"
-		: process.env.NEXT_PUBLIC_NODE_ENV === "staging" &&
-		  typeof window !== "undefined"
-		? window.location.origin
-		: "https://paper.xyz";
-
-export const PAPER_APP_URL_ALT =
-	process.env.NEXT_PUBLIC_NODE_ENV === "development" ||
-	process.env.NODE_ENV === "development"
-		? "http://localhost:3000"
-		: process.env.NEXT_PUBLIC_NODE_ENV === "staging" &&
+		: process?.env?.NEXT_PUBLIC_NODE_ENV === "staging" &&
 		  typeof window !== "undefined"
 		? window.location.origin
 		: "https://withpaper.com";
